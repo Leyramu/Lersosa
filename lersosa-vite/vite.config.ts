5f3efc5550bc720dccf958516c94ebc21ce2a71e
Miraitowa_zcx <2038322151@qq.com>
2024-10-24 11:50:10 +0800

    2024-10-24-11:50 前端添加工具类

--- lersosa-vite/vite.config.ts
+++ lersosa-vite/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig, loadEnv, UserConfigExport} from 'vite';
+import path from 'path';
+import createVitePlugins from './vite/plugins';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({mode, command}: { mode: string; command: string }): UserConfigExport => {
+    const env = loadEnv(mode, process.cwd());
+    const {VITE_APP_ENV} = env;
+    return {
+        base: VITE_APP_ENV === 'production' ? '/' : '/',
+        plugins: createVitePlugins(env, command === 'build'),
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, './'),
+                '@': path.resolve(__dirname, './src')
+            },
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+        },
+        server: {
+            port: 80,
+            host: true,
+            open: true,
+            proxy: {
+                '/dev-api': {
+                    target: 'http://localhost:8080',
+                    changeOrigin: true,
+                    rewrite: (p: string) => p.replace(/^\/dev-api/, '')
+                }
+            }
+        },
+        css: {
+            postcss: {
+                plugins: [
+                    {
+                        postcssPlugin: 'internal:charset-removal',
+                        AtRule: {
+                            charset: (atRule: any) => {
+                                if (atRule.name === 'charset') {
+                                    atRule.remove();
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    };
+});
