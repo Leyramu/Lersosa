1c8a684e6d6520de368d6d1566480c88ec4346f1
Miraitowa_zcx <2038322151@qq.com>
2025-03-02 12:02:11 +0800

    2025-03-02-12:02 添加 Grpc 构建自动脚本

--- doc/build/build-grpc.bat
+++ doc/build/build-grpc.bat
@@ -1,8 +1,23 @@
 #
-# Copyright (c) 2024 Leyramu. All rights reserved.
+# Copyright (c) 2025 Leyramu Group. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # This project (Lersosa), including its source code, documentation, and any associated materials, is the intellectual property of Leyramu. No part of this software may be reproduced, distributed, or transmitted in any form or by any means, including photocopying, recording, or other electronic or mechanical methods, without the prior written permission of the copyright owner, Miraitowa_zcx, except in the case of brief quotations embodied in critical reviews and certain other noncommercial uses permitted by copyright law.
+#
 # For inquiries related to licensing or usage outside the scope of this notice, please contact the copyright holder at 2038322151@qq.com.
+#
 # The author disclaims all warranties, express or implied, including but not limited to the warranties of merchantability and fitness for a particular purpose. Under no circumstances shall the author be liable for any special, incidental, indirect, or consequential damages arising from the use of this software.
+#
 # By using this project, users acknowledge and agree to abide by these terms and conditions.
 #
 
@@ -11,7 +26,11 @@
 cd /d %~dp0/../..
 set rootDir=%cd%
 
-docker-compose -f "%rootDir%/docker/docker-compose-dev.yml" --project-name leyramu_lersosa_dev up
+set /p protoName=Please enter the name of the service:
+
+cd %rootDir%/lersosa-python/src/proto/
+
+python -m grpc_tools.protoc -I./rpc --python_out=. --grpc_python_out=. ./rpc/%protoName%.proto
 
 echo Done
 pause
